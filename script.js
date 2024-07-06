4d0dc3be926a8d9bbab3560f68e7148ad062bd19
AbdurRahmanShanto <arshanto5649@gmail.com>
2024-07-06 12:01:26 +0600

    tailwind

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-document.getElementById('theme-toggle').addEventListener('click', function() {
-document.body.classList.toggle('dark-mode');
-});
-document.getElementById('theme-toggle').addEventListener('click', function() {
-document.getElementById('target-section').classList.toggle('dark-mode');
- });
+      document.getElementById('theme-toggle').addEventListener('click', function() {
+      document.body.classList.toggle('dark-mode');
+      });
+      document.getElementById('theme-toggle').addEventListener('click', function() {
+      document.getElementById('target-section').classList.toggle('dark-mode');
+      });
